3f6336c996e707d2d89a12cef1cbeae99f4b5dd4
debchung <debchung@umich.edu>
2022-11-30 13:37:57 -0500

    2nd commit

--- discussion_12.py
+++ discussion_12.py
@@ -16,7 +16,7 @@ def setUpDatabase(db_name):
 # TASK 1
 # CREATE TABLE FOR EMPLOYEE INFORMATION IN DATABASE AND ADD INFORMATION
 def create_employee_table(cur, conn):
-    cur.execute("CREAT TABLE IF NOT EXISTS Employees")
+    cur.execute('CREATE TABLE IF NOT EXISTS employees (employee_id INTEGER PRIMARY KEY, first_name TEXT, last_name TEXT, hire_date TEXT, job_id INTEGER, salary INTEGER)')
     conn.commit()
 
 # ADD EMPLOYEE'S INFORMTION TO THE TABLE
@@ -36,7 +36,7 @@ def add_employee(filename, cur, conn):
         hire_date = item('hire_date')
         job_id = int(item('job_id'))
         salary = int(item('salary'))
-        cur.execute("ISNERT INTO Employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)" (employee_id, first_name, last_name, hire_date, job_id, salary))
+        cur.execute('INSERT OR IGNORE INTO employees (employee_id, first_name, last_name, hire_date, job_id, salary) VALUES (?,?,?,?,?,?)', (employee_id, first_name, last_name, hire_date, job_id, salary))
     conn.commit()
 # TASK 2: GET JOB AND HIRE_DATE INFORMATION
 def job_and_hire_date(cur, conn):
